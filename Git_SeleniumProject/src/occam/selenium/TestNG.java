73ac9dd257d4447e73be8c53f8a56523b18429d5
occamrazzor <occam_razor@hotmail.com>
2017-10-18 00:14:39 -0700

    @BeforeMethod - Runs after each @Test @AfterTest - Runs after all @Test complete. @BeforeTest - Runs before any @Test starts.

--- Git_SeleniumProject/src/occam/selenium/TestNG.java
+++ Git_SeleniumProject/src/occam/selenium/TestNG.java
@@ -1,12 +1,51 @@
 package occam.selenium;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class TestNG{
-	
-	@Test
-	public void OpenBrowser(){
-		System.out.println("TestNG");
+// BeforeMethod will run before each @Test executes 
+	@BeforeMethod
+	public void RunningBeforeTest(){
+		System.out.println("@BeforeMethod Delete cookies before each test");
+	}
+	@BeforeTest
+	public void BeforeTesting(){
+		System.out.println("@BeforeTest Delete cookies before starting test");
+	}
+	@AfterTest
+	public void AfterTesting(){
+		System.out.println("@AfterTest Delete cookies after test completion");
 	}
 	
-
+	@Test
+	public void TestNumber1(){
+		System.out.println("Selecting Element #1");
+	}
+	
+	@Test
+	public void TestNumber2(){
+		System.out.println("Selecting Element #2");
+	
+	}
+	
+	@Test
+	public void TestNumber3(){
+		System.out.println("Selecting Element #3");
+	
+	}
+	
+	@Test
+	public void TestNumber4(){
+		System.out.println("Selecting Element #4");
+	
+	}
+	
+	@Test
+	public void TestNumber5(){
+		System.out.println("Selecting Element #5");
+	
+	}
+	
 }
